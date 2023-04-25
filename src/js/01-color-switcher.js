5f340f83e1336360ee6c403d97d03ea575e6beac
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-04-25 22:31:37 +0300

    01-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,9 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const bodyEl = document.querySelector('body');
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
@@ -17,10 +23,4 @@ function startClick() {
 function stopClick() {
   btnStart.removeAttribute('disabled');
   clearInterval(changeColor);
-}
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
 }
